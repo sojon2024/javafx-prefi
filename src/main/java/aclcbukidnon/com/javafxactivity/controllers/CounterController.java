41a0d561bb530fa70d035d30cdc0b9506aea385a
Manuel-PC <Manuel-PC@MANZMARK-PC>
2025-04-04 14:00:13 +0800

    Reset the methods

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -12,18 +12,12 @@ public class CounterController {
 
     @FXML
     protected void onPlusClick() {
-        var countText = labelCount.getText();
-        var countValue = Integer.parseInt(countText);
-        countValue = countValue + 1;
-        labelCount.setText(countValue + "");
+
     }
 
     @FXML
     protected void onMinusClick() {
-        var countText = labelCount.getText();
-        var countValue = Integer.parseInt(countText);
-        countValue = countValue - 1;
-        labelCount.setText(countValue + "");
+
     }
 
 
