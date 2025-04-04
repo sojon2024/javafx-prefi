1e5951e61617639d869e8b712f7976168965532e
Manuel-PC <Manuel-PC@MANZMARK-PC>
2025-04-04 13:57:55 +0800

    First Commit

---
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -0,0 +1,30 @@
+package aclcbukidnon.com.javafxactivity.controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CounterController {
+
+    @FXML
+    private Label labelCount;
+
+
+    @FXML
+    protected void onPlusClick() {
+        var countText = labelCount.getText();
+        var countValue = Integer.parseInt(countText);
+        countValue = countValue + 1;
+        labelCount.setText(countValue + "");
+    }
+
+    @FXML
+    protected void onMinusClick() {
+        var countText = labelCount.getText();
+        var countValue = Integer.parseInt(countText);
+        countValue = countValue - 1;
+        labelCount.setText(countValue + "");
+    }
+
+
+}
